a087833c57e28cc267e5da759bf8b3129c5ff2c1
hhamroush <63880437+hhamroush@users.noreply.github.com>
2023-05-06 18:29:30 -0300

    Header complete, minor styling fixes left

--- src/components/header.js
+++ src/components/header.js
@@ -1,47 +1,340 @@
-import React from 'react';
+// import React from 'react';
+// import tailwind from 'tailwindcss';
 
-function Header(){
-    return(
-            <header>
-                <div class="mainHeader">
-                    <div class="mainHeaderTop">
-                        <ul class="mainHeaderAbout">
-                            <li>
-                                <div class="icon">
-                                    <span class="icon-pin"></span>
-                                </div>
-                                <div class="text">
-                                    <p>
-                                        3-5 Mousadak Street El Nahda Tower - 12th &amp; 13th floor
-                                    </p>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="icon">
-                                    <span class="icon-email"></span>
-                                </div>
-                                <div class="text">
-                                    <p>
-	                                    <a href="mailto:info@ament-egypt.com">info@ament-egypt.com</a>
-                                    </p>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="text">
-                                    <p><a href="https://makhzankey.com/ament/language?lang=ar">  عربي</a></p>
-                                </div>
-                            </li>
-                        </ul>
-                        <div class="mainHeaderSocials">
-                        <a href="#" target="_blank" ><i class="fab fa-facebook-f"></i></a>
-                        <a href="#" target="_blank" ><i class="fab fa-youtube"></i></a>
-                        <a href="#" target="_blank" ><i class="fab fa-instagram"></i></a>
-                        <a href="https://www.linkedin.com/company/ament/mycompany/?viewAsMember=true" target="_blank" ><i class="fab fa-linkedin-in"></i></a>
-                        <a href="https://api.whatsapp.com/send?phone=+20111111" target="_blank" ><i class="fab fa-whatsapp"></i></a>
-                        </div>          
-                    </div>
-                </div>
-            </header>
-    );    
+
+// function Header(){
+//     return(
+//         <header>
+//             <div className="mainHeader">
+//                 <div className="mainHeaderTop">
+//                 <ul className="mainHeaderAbout">
+//                     <li>
+//                     <div className="icon">
+//                         <span className="icon-pin" />
+//                     </div>
+//                     <div className="text">
+//                         <p>3-5 Mousadak Street El Nahda Tower - 12th &amp; 13th floor</p>
+//                     </div>
+//                     </li>
+//                     <li>
+//                     <div className="icon">
+//                         <span className="icon-email" />
+//                     </div>
+//                     <div className="text">
+//                         <p>
+//                         <a href="mailto:info@ament-egypt.com">info@ament-egypt.com</a>
+//                         </p>
+//                     </div>
+//                     </li>
+//                     <li>
+//                     <div className="text">
+//                         <p>
+//                         <a href="https://makhzankey.com/ament/language?lang=ar"> عربي</a>
+//                         </p>
+//                     </div>
+//                     </li>
+//                 </ul>
+//                 <div className="mainHeaderSocials">
+//                     <a href="#" target="_blank">
+//                     <i className="fab fa-facebook-f" />
+//                     </a>
+//                     <a href="#" target="_blank">
+//                     <i className="fab fa-youtube" />
+//                     </a>
+//                     <a href="#" target="_blank">
+//                     <i className="fab fa-instagram" />
+//                     </a>
+//                     <a
+//                     href="https://www.aedin.com/company/ament/mycompany/?viewAsMember=true"
+//                     target="_blank"
+//                     >
+//                     <i className="fab fa-aedin-in" />
+//                     </a>
+//                     <a href="https://api.whatsapp.com/send?phone=+20111111" target="_blank">
+//                     <i className="fab fa-whatsapp" />
+//                     </a>
+//                 </div>
+//                 </div>
+//             </div>
+//             <div className="navbar">
+//                 <ul>
+//                     <li><a href="#">Home</a></li>
+//                     <li><a href="#">Overview</a></li>
+//                     <li><a href="#">Services</a></li>
+//                     <li><a href="#">Products</a></li>
+//                     <li><a href="#">Projects</a></li>
+//                     <li><a href="#">Partners</a></li>
+//                     <li><a href="#">Contact Us</a></li>
+//                 </ul>
+//             </div>
+//         </header>
+//     );    
+// }
+// export default Header;
+
+import { Fragment, useState } from 'react'
+import { Dialog, Disclosure, Popover, Transition } from '@headlessui/react'
+import {
+  ArrowPathIcon,
+  Bars3Icon,
+  ChartPieIcon,
+  CursorArrowRaysIcon,
+  FingerPrintIcon,
+  SquaresPlusIcon,
+  XMarkIcon,
+} from '@heroicons/react/24/outline'
+import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
+//Objects 
+const products = [
+
+
+  { name: 'Analytics', description: 'Get a better understanding of your traffic', href: '#', icon: ChartPieIcon },
+  { name: 'Engagement', description: 'Speak directly to your customers', href: '#', icon: CursorArrowRaysIcon },
+  { name: 'Security', description: 'Your customers’ data will be safe and secure', href: '#', icon: FingerPrintIcon },
+  { name: 'Integrations', description: 'Connect with third-party tools', href: '#', icon: SquaresPlusIcon },
+  { name: 'Automations', description: 'Build strategic funnels that will convert', href: '#', icon: ArrowPathIcon },
+]
+const callsToAction = [
+
+
+  { name: 'Watch demo', href: '#', icon: PlayCircleIcon },
+  { name: 'Contact sales', href: '#', icon: PhoneIcon },
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
 }
-export default Header;
+
+export default function Header() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
+  return (
+    <header>
+      
+        <div className="h-25 mx-auto flex w-100 justify-end p-2 lg:px-5 bg-black text-white">
+                     <a href="#" target="_blank" className="mx-2">
+                     <i className="fab fa-facebook-f" />
+                     Facebook
+                     </a>
+                     <a href="#" target="_blank" className="mx-2">
+                     Youtube
+                     <i className="fab fa-youtube" />
+                     </a>                     
+                     <a href="#" target="_blank" className="mx-2">
+                     Intagram
+                     <i className="fab fa-instagram" />
+                     </a>
+                     <a
+                     href="https://www.aedin.com/company/ament/mycompany/?viewAsMember=true"
+                     target="_blank" className="mx-2"
+                     >
+                     WhatsApp
+                     <i className="fab fa-aedin-in" />                   
+                     </a>
+        </div>
+
+      <nav className="mx-auto flex w-100 items-center justify-between p-6 lg:px-8 text-white bg-black" aria-label="Global">
+        <div className="flex lg:flex-1">
+          <a href="#" className="-m-1.5 p-1.5">
+            <span className="sr-only">Your Company</span>
+            <img className="h-8 w-auto" src="../images/logo-1.png" alt="" />
+          </a>
+        </div>
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            onClick={() => setMobileMenuOpen(true)}
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
+        {/* THE ENTIRE POP DOWN MENU - Entire popOver group is used for the nav options */}
+        <Popover.Group className="hidden lg:flex lg:gap-x-12">
+          <a href="#" className="text-sm font-semibold leading-6">
+            Home
+          </a>
+          <a href="#" className="text-sm font-semibold leading-6">
+            Overview
+          </a>
+          <a href="#" className="text-sm font-semibold leading-6">
+            Services
+          </a>
+            {/* THat entire Popover is the drop down menu */}
+          <Popover className="relative">
+            <Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6">
+              Products
+              <ChevronDownIcon className="h-5 w-5 flex-none" aria-hidden="true" />
+            </Popover.Button>
+
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-200"
+              enterFrom="opacity-0 translate-y-1"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-150"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-1"
+            >
+              <Popover.Panel className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
+                <div className="p-4">
+                  {products.map((item) => (
+                    <div
+                      key={item.name}
+                      className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
+                    >
+                      <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+                        <item.icon className="h-6 w-6 text-gray-600 group-hover:text-indigo-600" aria-hidden="true" />
+                      </div>
+                      <div className="flex-auto">
+                        <a href={item.href} className="block font-semibold text-gray-900">
+                          {item.name}
+                          <span className="absolute inset-0" />
+                        </a>
+                        <p className="mt-1 text-gray-600">{item.description}</p>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </Popover.Panel>
+            </Transition>
+          </Popover>
+
+          <Popover className="relative">
+            <Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6">
+              Projects
+              <ChevronDownIcon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
+            </Popover.Button>
+
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-200"
+              enterFrom="opacity-0 translate-y-1"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-150"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-1"
+            >
+              <Popover.Panel className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
+                <div className="p-4">
+                  {products.map((item) => (
+                    <div
+                      key={item.name}
+                      className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
+                    >
+                      <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+                        <item.icon className="h-6 w-6 text-gray-600 group-hover:text-indigo-600" aria-hidden="true" />
+                      </div>
+                      <div className="flex-auto">
+                        <a href={item.href} className="block font-semibold text-gray-900">
+                          {item.name}
+                          <span className="absolute inset-0" />
+                        </a>
+                        <p className="mt-1">{item.description}</p>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              
+              </Popover.Panel>
+            </Transition>
+          </Popover>
+
+          <a href="#" className="text-sm font-semibold leading-6">
+            Partners
+          </a>
+          <a href="#" className="text-sm font-semibold leading-6">
+            Contact Us
+          </a>
+        </Popover.Group>
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+          {/* <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+            Log in <span aria-hidden="true">&rarr;</span>
+          </a> */}
+        </div>
+      </nav>
+      <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+        <div className="fixed inset-0 z-10" />
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Your Company</span>
+              <img
+                className="h-8 w-auto"
+                src="../images/logo-1.png"
+                alt=""
+              />
+            </a>
+            <button
+              type="button"
+              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+                <Disclosure as="div" className="-mx-3">
+                  {({ open }) => (
+                    <>
+                      <Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 hover:bg-gray-50">
+                        Product
+                        <ChevronDownIcon
+                          className={classNames(open ? 'rotate-180' : '', 'h-5 w-5 flex-none')}
+                          aria-hidden="true"
+                        />
+                      </Disclosure.Button>
+                      <Disclosure.Panel className="mt-2 space-y-2">
+                        {[...products, ...callsToAction].map((item) => (
+                          <Disclosure.Button
+                            key={item.name}
+                            as="a"
+                            href={item.href}
+                            className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                          >
+                            {item.name}
+                          </Disclosure.Button>
+                        ))}
+                      </Disclosure.Panel>
+                    </>
+                  )}
+                </Disclosure>
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Features
+                </a>
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Marketplace
+                </a>
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Company
+                </a>
+              </div>
+              <div className="py-6">
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Log in
+                </a>
+              </div>
+            </div>
+          </div>
+        </Dialog.Panel>
+      </Dialog>
+    </header>
+  )
+}
+
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
 import Header from './components/header';
 
 
@@ -12,6 +13,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header />
+
+
   </React.StrictMode>
 );
 
