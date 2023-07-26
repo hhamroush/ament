203b4ddf002afa8dc03eb39fcbcba4b3b3efc82b
hhamroush <63880437+hhamroush@users.noreply.github.com>
2023-07-26 14:31:52 -0300

    NavBar redo in progress

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,5 @@
 import { Fragment, useState } from 'react'
+import { Link } from 'react-router-dom';
 import { Dialog, Disclosure, Popover, Transition } from '@headlessui/react'
 import {
   ArrowPathIcon,
@@ -12,16 +13,19 @@ import {
 import { SocialIcon } from 'react-social-icons'
 
 import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
-//Objects 
+import {FaLocationPin} from 'react-icons/fa';
+//Objects for the Products dropdown
 const products = [
 
 
   { name: 'Water & Wastewater Equipment', href: '#', icon: SocialIcon },
-  { name: 'Engagement', description: 'Speak directly to your customers', href: '#', icon: SocialIcon },
-  { name: 'Security', description: 'Your customers’ data will be safe and secure', href: '#', icon: SocialIcon },
-  { name: 'Integrations', description: 'Connect with third-party tools', href: '#', icon: SocialIcon },
-  { name: 'Automations', description: 'Build strategic funnels that will convert', href: '#', icon: SocialIcon },
+  { name: 'Pumps', href: '#', icon: SocialIcon },
+  { name: 'Motors & Gearboxes', href: '#', icon: SocialIcon },
+  { name: 'Valves', href: '#', icon: SocialIcon },
 ]
+
+
+
 const callsToAction = [
 
 
@@ -39,134 +43,27 @@ export default function Header() {
   return (
     <header>
       
+        <div classNmae="">
+         
+        </div>
         <div className="h-25 mx-auto flex w-100 justify-end p-2 lg:px-5 bg-gray-700 text-white">
             <SocialIcon url="https://www.linkedin.com/company/ament/?viewAsMember=true" style={{ height: 25, width: 25, }} bgColor="black"/>
             <SocialIcon url="https://www.facebook.com" style={{ height: 25, width: 25, }}/>
             <SocialIcon url="https://www.whatsapp.com" style={{ height: 25, width: 25, }}/>
                      
         </div>
+        <nav>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Overview</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Products</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#">Partners</a></li>
+            <li><a href="#">Contact Us</a></li>
+          </ul>
+        </nav>
 
-      <nav className="mx-auto flex w-100 items-center justify-between lg:px-8 text-white bg-black" aria-label="Global">
-        <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
-            <span className="sr-only">Your Company</span>
-            <img className="h-8 w-auto" src="../images/logo-1.png" alt="" />
-          </a>
-        </div>
-        <div className="flex lg:hidden">
-          <button
-            type="button"
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-            onClick={() => setMobileMenuOpen(true)}
-          >
-            <span className="sr-only">Open main menu</span>
-            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-          </button>
-        </div>
-        {/* Entire Popover.Group is used for the nav options */}
-        <Popover.Group className="hidden lg:flex lg:gap-x-12">
-          <a href="#" className="text-sm font-semibold leading-6 hover:bg-gray-700 p-6">
-            Home
-          </a>
-          <a href="#" className="text-sm font-semibold leading-6 hover:bg-gray-700 p-6">
-            Overview
-          </a>
-          <a href="#" className="text-sm font-semibold leading-6 hover:bg-gray-700 p-6">
-            Services
-          </a>
-            {/* THE ENTIRE POP DOWN MENU - entire Popover is the drop down menu */}
-          <Popover className="relative">
-            <Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6 hover:bg-gray-700 p-6">
-              Products
-              <ChevronDownIcon className="h-5 w-5 flex-none" aria-hidden="true" />
-            </Popover.Button>
-
-            <Transition
-              as={Fragment}
-              enter="transition ease-out duration-200"
-              enterFrom="opacity-0 translate-y-1"
-              enterTo="opacity-100 translate-y-0"
-              leave="transition ease-in duration-150"
-              leaveFrom="opacity-100 translate-y-0"
-              leaveTo="opacity-0 translate-y-1"
-            >
-              <Popover.Panel className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
-                <div className="p-4">
-                  {products.map((item) => (
-                    <div
-                      key={item.name}
-                      className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
-                    >
-                      <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                        <item.icon className="h-6 w-6 text-gray-600 group-hover:text-indigo-600" aria-hidden="true" />
-                      </div>
-                      <div className="flex-auto">
-                        <a href={item.href} className="block font-semibold text-gray-900">
-                          {item.name}
-                          <span className="absolute inset-0" />
-                        </a>
-                        <p className="mt-1 text-gray-600">{item.description}</p>
-                      </div>
-                    </div>
-                  ))}
-                </div>
-              </Popover.Panel>
-            </Transition>
-          </Popover>
-
-          <Popover className="relative">
-            <Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6 hover:bg-gray-700 p-6">
-              Projects
-              <ChevronDownIcon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
-            </Popover.Button>
-
-            <Transition
-              as={Fragment}
-              enter="transition ease-out duration-200"
-              enterFrom="opacity-0 translate-y-1"
-              enterTo="opacity-100 translate-y-0"
-              leave="transition ease-in duration-150"
-              leaveFrom="opacity-100 translate-y-0"
-              leaveTo="opacity-0 translate-y-1"
-            >
-              <Popover.Panel className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
-                <div className="p-4">
-                  {products.map((item) => (
-                    <div
-                      key={item.name}
-                      className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
-                    >
-                      <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                        <item.icon className="h-6 w-6 text-gray-600 group-hover:text-indigo-600" aria-hidden="true" />
-                      </div>
-                      <div className="flex-auto">
-                        <a href={item.href} className="block font-semibold text-gray-900">
-                          {item.name}
-                          <span className="absolute inset-0" />
-                        </a>
-                        <p className="mt-1">{item.description}</p>
-                      </div>
-                    </div>
-                  ))}
-                </div>
-              
-              </Popover.Panel>
-            </Transition>
-          </Popover>
-
-          <a href="#" className="text-sm font-semibold leading-6 hover:bg-gray-700 p-6">
-            Partners
-          </a>
-          <a href="#" className="text-sm font-semibold leading-6 hover:bg-gray-700 p-6">
-            Contact Us
-          </a>
-        </Popover.Group>
-        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          {/* <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-            Log in <span aria-hidden="true">&rarr;</span>
-          </a> */}
-        </div>
-      </nav>
       <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
         <div className="fixed inset-0 z-10" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
